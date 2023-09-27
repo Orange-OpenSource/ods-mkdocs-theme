a40028d29f4ac75fc7278738bb5bdfd003b9479b
Julien Déramond <julien.deramond@orange.com>
2023-09-27 13:26:25 +0200

    feat: import ods-mkdoc-theme
    Co-authored-by: Loïc Laussel <loic.laussel@orange.com>

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+setup(
+  name='ods-mkdocs-theme',
+  version=VERSION,
+  url='https://github.com/Orange-OpenSource/ods-mkdocs-theme',
+  license='MIT',
+  description='Orange Design System MkDocs Theme provides a MkDocs theme for Orange.',
+  author='Orange',
+  author_email='loic.laussel@orange.com, julien.deramond@orange.com',
+  packages=find_packages(),
+  include_package_data=True,
+  install_requires=[
+    'mkdocs-material>=9.0.3'
+  ],
+  python_requires='>=3.7',
+  entry_points={
+      'mkdocs.themes': [
+        'ods-mkdocs-theme = ods_mkdocs_theme',
+      ]
+  },
+  zip_safe=False
+)
